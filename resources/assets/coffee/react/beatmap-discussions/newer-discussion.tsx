724d5c4f705b5cce6096fbe7a73aebaeedb8271a
Jamie Taylor <me@nekodex.net>
2019-09-19 16:41:27 +0900

    slatejs wip

--- resources/assets/coffee/react/beatmap-discussions/newer-discussion.tsx
+++ resources/assets/coffee/react/beatmap-discussions/newer-discussion.tsx
@@ -16,33 +16,35 @@
  *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { AtomicBlockUtils, CompositeDecorator, convertToRaw, Editor, EditorState } from 'draft-js';
 import * as React from 'react';
+import { Value } from 'slate';
+import { Editor } from 'slate-react';
 
-// import ContentBlock = Draft.Model.ImmutableData.ContentBlock;
-// import DraftEditor = Draft.base
-// import { Post } from 'react/beatmap-discussions/post';
+// interface State {
+//   editorState: EditorState;
+// }
 
-interface State {
-  editorState: EditorState;
-}
+// interface Props {
+//   beatmapset: any;
+//   discussion: any;
+//   users: any;
+// }
 
-interface Props {
-  beatmapset: any;
-  discussion: any;
-  users: any;
-}
+const existingValue: any = localStorage.getItem('content') || '{"document":{"nodes":[{"object":"block","type":"paragraph","nodes":[{"object":"text","text":"A line of text in a paragraph."}]}]}}';
+
+const initialValue = Value.fromJSON(JSON.parse(existingValue));
 
 class TestComponent extends React.Component<any, any> {
+  remove = (event) => {
+    const { editor, node } = this.props;
+
+    event.preventDefault();
+    editor.removeNodeByKey(node.key);
+  }
+
   render(): React.ReactNode {
-    const {block, contentState} = this.props;
-    const {foo} = this.props.blockProps;
-    const data = contentState.getEntity(block.getEntityAt(0)).getData();
-
-    console.log('data', this.props, data, foo);
-
     return (
-      <div className="beatmap-discussion beatmap-discussion--preview">
+      <div className="beatmap-discussion beatmap-discussion--preview" {...this.props.attributes}>
           <div className="beatmap-discussion__discussion">
               <div className="beatmap-discussion-post beatmap-discussion-post--reply beatmap-discussion-post--dev">
                   <div className="beatmap-discussion-post__content">
@@ -71,23 +73,19 @@ class TestComponent extends React.Component<any, any> {
                       </div>
                       <div className="beatmap-discussion-post__message-container undefined">
                           <div className="beatmap-discussion-post__message">
-                              <div className="beatmapset-discussion-message">lol placeholder</div>
+                              <div className="beatmapset-discussion-message">{this.props.children}</div>
                           </div>
                           <div className="beatmap-discussion-post__info-container">
                               <span className="beatmap-discussion-post__info">
-                                  <time className="timeago" dateTime="2019-09-05T08:04:17+00:00" title="2019-09-05T08:04:17+00:00">8 days ago</time>
+                                  {/*<time className="timeago" dateTime="2019-09-05T08:04:17+00:00" title="2019-09-05T08:04:17+00:00">8 days ago</time>*/}
                               </span>
                           </div>
-{/*
                           <div className="beatmap-discussion-post__actions">
                               <div className="beatmap-discussion-post__actions-group">
-                                  <button className="beatmap-discussion-post__action beatmap-discussion-post__action--button">edit</button>
-                                  <a className="js-beatmapset-discussion-update beatmap-discussion-post__action beatmap-discussion-post__action--button"
-                                     href="/beatmapsets/beatmap-discussion-posts/3203143" data-remote="true"
-                                     data-method="DELETE" data-confirm="Are you sure?">delete</a>
+                                  {/*<button className="beatmap-discussion-post__action beatmap-discussion-post__action--button">edit</button>*/}
+                                  <a className="beatmap-discussion-post__action beatmap-discussion-post__action--button" href="#" onClick={this.remove}>delete</a>
                               </div>
                           </div>
-*/}
                       </div>
                   </div>
               </div>
@@ -108,40 +106,39 @@ class TestComponent extends React.Component<any, any> {
     currentUser={currentUser}
     />
  */
-export default class NewerDiscussion extends React.Component<any, State> {
-  // private HANDLE_REGEX = /@[\w]+/g;
-  // private HASHTAG_REGEX = /#[\w\u0590-\u05ff]+/g;
-  private TIMESTAMP_REGEX = /\b(\d{2,}):([0-5]\d)[:.](\d{3})\b/g;
-  private readonly onChange: (editorState: EditorState) => void;
+export default class NewerDiscussion extends React.Component<any, any> {
+  // private TIMESTAMP_REGEX = /\b(\d{2,}):([0-5]\d)[:.](\d{3})\b/g;
+
+  editor = React.createRef<Editor>();
 
   constructor(props: {}) {
     super(props);
 
-    const compositeDecorator = new CompositeDecorator([
-      // {
-      //   component: this.HandleSpan,
-      //   strategy: this.handleStrategy,
-      // },
-      // {
-      //   component: this.HashtagSpan,
-      //   strategy: this.hashtagStrategy,
-      // },
-      {
-        component: this.TimestampSpan,
-        strategy: this.timestampStrategy,
-      },
-    ]);
+    // const compositeDecorator = new CompositeDecorator([
+    //   // {
+    //   //   component: this.HandleSpan,
+    //   //   strategy: this.handleStrategy,
+    //   // },
+    //   // {
+    //   //   component: this.HashtagSpan,
+    //   //   strategy: this.hashtagStrategy,
+    //   // },
+    //   {
+    //     component: this.TimestampSpan,
+    //     strategy: this.timestampStrategy,
+    //   },
+    // ]);
 
     this.state = {
-      editorState: EditorState.createEmpty(compositeDecorator),
+      value: initialValue,
     };
 
-    this.onChange = (editorState) => this.setState({editorState});
+    // this.onChange = (editorState) => this.setState({editorState});
   }
 
-  componentDidUpdate(prevProps: Readonly<any>, prevState: Readonly<State>, snapshot?: any): void {
-    console.log('waaaaaaaaaangs', prevProps, prevState, snapshot);
-  }
+  // componentDidUpdate(prevProps: Readonly<any>, prevState: Readonly<State>, snapshot?: any): void {
+  //   console.log('waaaaaaaaaangs', prevProps, prevState, snapshot);
+  // }
 
   // handleStrategy = (contentBlock: any, callback: any, contentState: any) => {
   //   this.findWithRegex(this.HANDLE_REGEX, contentBlock, callback);
@@ -151,18 +148,18 @@ export default class NewerDiscussion extends React.Component<any, State> {
   //   this.findWithRegex(this.HASHTAG_REGEX, contentBlock, callback);
   // }
 
-  timestampStrategy = (contentBlock: any, callback: any, contentState: any) => {
-    this.findWithRegex(this.TIMESTAMP_REGEX, contentBlock, callback);
-  }
-
-  findWithRegex = (regex: any, contentBlock: any, callback: any) => {
-    const text = contentBlock.getText();
-    let matchArr, start;
-    while ((matchArr = regex.exec(text)) !== null) {
-      start = matchArr.index;
-      callback(start, start + matchArr[0].length);
-    }
-  }
+  // timestampStrategy = (contentBlock: any, callback: any, contentState: any) => {
+  //   this.findWithRegex(this.TIMESTAMP_REGEX, contentBlock, callback);
+  // }
+  //
+  // findWithRegex = (regex: any, contentBlock: any, callback: any) => {
+  //   const text = contentBlock.getText();
+  //   let matchArr, start;
+  //   while ((matchArr = regex.exec(text)) !== null) {
+  //     start = matchArr.index;
+  //     callback(start, start + matchArr[0].length);
+  //   }
+  // }
 
   // HandleSpan = (props: any) => {
   //   return (
@@ -211,34 +208,72 @@ export default class NewerDiscussion extends React.Component<any, State> {
   }
 
   buttan = () => {
-    const contentState = this.state.editorState.getCurrentContent();
-    const contentStateWithEntity = contentState.createEntity(
-      'test',
-      'IMMUTABLE',
-      {},
-    );
-    const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
-    const newEditorState = AtomicBlockUtils.insertAtomicBlock(
-      this.state.editorState,
-      entityKey,
-      ' ',
-    );
+    if (!this.editor.current) {
+      return;
+    }
 
-    this.setState({
-      editorState: EditorState.forceSelection(
-        newEditorState,
-        newEditorState.getCurrentContent().getSelectionAfter(),
-      ),
-    });
+    this.editor.current
+      .insertBlock({
+        type: 'embed',
+      })
+      .focus();
+
+    // value.change()
+    //   .insertBlock({
+    //     type: 'embed',
+    //     data: {
+    //       foo: 'bar',
+    //     },
+    //   });
+    // const contentState = this.state.editorState.getCurrentContent();
+    // const contentStateWithEntity = contentState.createEntity(
+    //   'test',
+    //   'IMMUTABLE',
+    //   {},
+    // );
+    // const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
+    // const newEditorState = AtomicBlockUtils.insertAtomicBlock(
+    //   this.state.editorState,
+    //   entityKey,
+    //   ' ',
+    // );
+    //
+    // this.setState({
+    //   editorState: EditorState.forceSelection(
+    //     newEditorState,
+    //     newEditorState.getCurrentContent().getSelectionAfter(),
+    //   ),
+    // });
+  }
+
+  renderBlock = (props, editor, next) => {
+    switch (props.node.type) {
+      case 'embed':
+        return <TestComponent {...props} />;
+      default:
+        return next();
+    }
   }
 
   log = () => {
-    console.log(convertToRaw(this.state.editorState.getCurrentContent()));
+    console.log(
+      'LOGGGGGGGG',
+      this.state.value.toJSON(),
+    );
+    // console.log(convertToRaw(this.state.editorState.getCurrentContent()));
+  }
+
+  // @ts-ignore
+  onChange = ({ value }) => {
+    const content = JSON.stringify(value.toJSON());
+    localStorage.setItem('content', content);
+
+    this.setState({ value });
   }
 
   render(): React.ReactNode {
     const cssClasses = 'beatmap-discussion-new-float';
-    const bn = 'beatmap-discussion-new';
+    const bn = 'beatmap-discussion-newer';
 
     return (
       <div className={cssClasses}>
@@ -249,21 +284,39 @@ export default class NewerDiscussion extends React.Component<any, State> {
                 <div className='page-title'>{osu.trans('beatmaps.discussions.new.title')}</div>
                 <div className={`${bn}__content`}>
                   <Editor
-                    editorState={this.state.editorState}
+                    value={this.state.value}
                     onChange={this.onChange}
-                    placeholder='WANG ALL THE CHUNGS'
-                    blockRendererFn={this.myBlockRenderer}
-                    // ref={this.ref}
+                    // editorState={this.state.editorState}
+                    // onChange={this.onChange}
+                    // placeholder='WANG ALL THE CHUNGS'
+                    // blockRendererFn={this.myBlockRenderer}
+                    renderBlock={this.renderBlock}
+                    ref={this.editor}
                   />
+                  <hr width='100%' />
+
+                  <div className="forum-post-edit__buttons forum-post-edit__buttons--actions">
+                    <div className="forum-post-edit__button">
+                        <button type="button" className="btn-osu-big btn-osu-big--forum-secondary" onClick={this.buttan}>
+                            buttan
+                        </button>
+                    </div>
+
+                    <div className="forum-post-edit__button forum-post-edit__button--preview">
+                        <button type="button" className="btn-osu-big btn-osu-big--forum-secondary" onClick={this.log}>
+                            log
+                        </button>
+                    </div>
+
+                      <div className="forum-post-edit__button">
+                          <button className="btn-osu-big btn-osu-big--forum-primary" type="submit" data-disable-with="Saving...">
+                              Post
+                          </button>
+                      </div>
+                  </div>
+                  {/*<button type='button' onClick={this.buttan}>buttan</button>*/}
+                  {/*<button type='button' onClick={this.log}>log</button>*/}
                 </div>
-                <button
-                  type='button'
-                  onClick={this.buttan}
-                >buttan</button>
-                <button
-                  type='button'
-                  onClick={this.log}
-                >log</button>
               </div>
             </div>
           </div>
